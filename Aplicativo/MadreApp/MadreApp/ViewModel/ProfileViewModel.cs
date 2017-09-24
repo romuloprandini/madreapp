83fcd6ce91aa9bf3cfde7e807057f50b331deaac
AlexCatarino <alexandre.catarino@gmail.com>
2017-09-25 00:21:32 +0100

    Implements Signup page

--- Aplicativo/MadreApp/MadreApp/ViewModel/ProfileViewModel.cs
+++ Aplicativo/MadreApp/MadreApp/ViewModel/ProfileViewModel.cs
@@ -1,95 +1,40 @@
-﻿using MadreApp.Behaviors;
-using MadreApp.Customs;
-using MadreApp.Helpers;
-using MadreApp.Pages;
+﻿using MadreApp.Helpers;
 using MvvmHelpers;
-using System.Windows.Input;
-using Xamarin.Forms;
-using System;
-using Newtonsoft.Json.Linq;
 
 namespace MadreApp.ViewModel
 {
     public class ProfileViewModel : BaseViewModel
     {
-        private string _birthday;
+        private string _phone;
+        private string _name;
         private string _email;
+        private string _gender;
+        private string _birthday;
         private string _fiscalNumber;
         private string _madreCardNumber;
         private bool _madreCardActive;
         private string _madreCardBalance;
-        private string _madreCardPassword;
-        private string _name;
-        private string _phone;
-        private ICommand _madreCardLoginCommand;
-        private ICommand _newUserLoginCommand;
-
-        public ICommand MadreCardLoginCommand => _madreCardLoginCommand ?? (_madreCardLoginCommand = new ButtonCommand(() =>
-            {
-                IsBusy = true;
-                Device.BeginInvokeOnMainThread(async () =>
-                    {
-                        var result = await HttpRequest.Instance.GetOneRequest<JObject>("/madrecard/" + Settings.FiscalNumber);
-
-                        Settings.MadreCardNumber = result["numero"].ToString();
-                        Settings.MadreCardActive = bool.Parse(result["ativo"].ToString());
-                        Settings.MadreCardBalance = result["saldo"].ToString();
-                        Settings.MadreCardPassword = MadreCardPassword;
-                        Settings.FiscalNumber = result["cpf"].ToString();
-                        Settings.Name = result["nome"].ToString();
-                        Settings.Phone = Phone;
-
-                        IsBusy = false;
-                        Application.Current.MainPage = new MainPage();
-                    });              
-            }, CanExecuteMadreCard, this));
-
-        public ICommand NewUserLoginCommand => _newUserLoginCommand ?? (_newUserLoginCommand = new ButtonCommand(() =>
-            {
-                Application.Current.MainPage = new MainPage();
-            }, CanExecuteNewUser, this));
-
+   
         public ProfileViewModel()
         {
-            _birthday = Settings.Birthday;
-            _email = Settings.Email;
-            _fiscalNumber = Settings.FiscalNumber;
+            _phone = Settings.Phone;
             _name = Settings.Name;
+            _email = Settings.Email;
+            _gender = Settings.Gender;
+            _birthday = Settings.Birthday;
+            _fiscalNumber = Settings.FiscalNumber;
             _madreCardNumber = Settings.MadreCardNumber;
             _madreCardActive = Settings.MadreCardActive;
-            _madreCardBalance = Settings.MadreCardBalance;
-            _madreCardPassword = Settings.MadreCardPassword;
-            _phone = Settings.Phone;
+            _madreCardBalance = Settings.MadreCardBalance;          
         }
 
-        public string Birthday
+        public string Phone
         {
-            get { return _birthday; }
+            get { return _phone; }
             set
             {
-                if (_birthday == value) return;
-                SetProperty(ref _birthday, value);
-            }
-        }
-
-        public string Email
-        {
-            get { return _email; }
-            set
-            {
-                if (_email == value) return;
-                SetProperty(ref _email, value);
-            }
-        }
-
-        public string FiscalNumber
-        {
-            get { return _fiscalNumber; }
-            set
-            {
-                if (_fiscalNumber == value) return;
-                SetProperty(ref _fiscalNumber, value);
-                
+                if (_phone == value) return;
+                SetProperty(ref _phone, value);
             }
         }
 
@@ -103,16 +48,46 @@ namespace MadreApp.ViewModel
             }
         }
 
-        public string Phone
+        public string Email
         {
-            get { return _phone; }
+            get { return _email; }
             set
             {
-                if (_phone == value) return;
-                SetProperty(ref _phone, value);
+                if (_email == value) return;
+                SetProperty(ref _email, value);
             }
         }
 
+        public string Gender
+        {
+            get { return _gender; }
+            set
+            {
+                if (_gender == value) return;
+                SetProperty(ref _gender, value);
+            }
+        }
+
+        public string Birthday
+        {
+            get { return _birthday; }
+            set
+            {
+                if (_birthday == value) return;
+                SetProperty(ref _birthday, value);
+            }
+        }
+
+        public string FiscalNumber
+        {
+            get { return _fiscalNumber; }
+            set
+            {
+                if (_fiscalNumber == value) return;
+                SetProperty(ref _fiscalNumber, value);              
+            }
+        }
+        
         public string MadreCardNumber
         {
             get { return _madreCardNumber; }
@@ -141,30 +116,6 @@ namespace MadreApp.ViewModel
                 if (_madreCardBalance == value) return;
                 SetProperty(ref _madreCardBalance, value);
             }
-        }
-
-        public string MadreCardPassword
-        {
-            get { return _madreCardPassword; }
-            set
-            {
-                if (_madreCardPassword == value) return;
-                SetProperty(ref _madreCardPassword, value);
-            }
-        }
-        
-        private bool CanExecuteMadreCard()
-        {
-            return
-                Validators.PhoneNumberValidator(Phone) &&
-                Validators.PasswordValidator(MadreCardPassword) &&
-                Validators.FiscalNumberValidator(FiscalNumber);
-        }
-
-        private bool CanExecuteNewUser()
-        {
-            return !string.IsNullOrWhiteSpace(Name) &&
-                Validators.PhoneNumberValidator(Phone);
         }
     }
 }
---
+++ Aplicativo/MadreApp/MadreApp/ViewModel/SignupViewModel.cs
@@ -0,0 +1,81 @@
+﻿using MadreApp.Behaviors;
+using MadreApp.Customs;
+using MadreApp.Helpers;
+using MadreApp.Pages;
+using System.Windows.Input;
+using Xamarin.Forms;
+using System;
+using Newtonsoft.Json.Linq;
+
+namespace MadreApp.ViewModel
+{
+    public class SignupViewModel : ProfileViewModel
+    {
+        private string _confirmationPassword;
+        private string _madreCardPassword;
+        private ICommand _signupCommand;
+
+        public string ConfirmationPassword
+        {
+            get { return _confirmationPassword; }
+            set
+            {
+                if (_confirmationPassword == value) return;
+                SetProperty(ref _confirmationPassword, value);
+            }
+        }
+
+        public string MadreCardPassword
+        {
+            get { return _madreCardPassword; }
+            set
+            {
+                if (_madreCardPassword == value) return;
+                SetProperty(ref _madreCardPassword, value);
+            }
+        }
+
+        public ICommand SignupCommand => _signupCommand ?? (_signupCommand = new ButtonCommand(() =>
+            {
+                IsBusy = true;
+                
+                Device.BeginInvokeOnMainThread(async () =>
+                    {
+                        try
+                        {
+                            var newUser = Settings.CreateNewUser(Phone, Name, Email, Gender, Birthday, FiscalNumber, MadreCardPassword);
+                            var result = await HttpRequest.Instance.GetOneRequest<JObject>("/usuario/" + newUser);
+
+                            Settings.Update(result);
+
+                            await Application.Current.MainPage?.DisplayAlert("Informação da Conta", Settings.GetAccountInformation(), "Ok");
+                            Application.Current.MainPage = new ProfilePage();
+                        }
+                        catch (Exception e)
+                        {
+                            await Application.Current.MainPage?.DisplayAlert("Error", e.Message, "Ok");
+                            Application.Current.MainPage = new MainPage();
+                        }
+                        IsBusy = false;
+                        
+                    });
+            }, CanSignup, this));
+
+        public SignupViewModel()
+            : base()
+        {
+            _madreCardPassword = Settings.MadreCardPassword;
+        }
+
+        private bool CanSignup()
+        {
+            return !string.IsNullOrWhiteSpace(Name) &&
+                Validators.PhoneNumberValidator(Phone) &&
+                Validators.FiscalNumberValidator(FiscalNumber) &&
+                Validators.BirthdayValidator(Birthday) &&
+                Validators.EmailValidator(Email) &&
+                Validators.PasswordValidator(MadreCardPassword) &&
+                MadreCardPassword == ConfirmationPassword;
+        }
+    }
+}
